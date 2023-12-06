f5d6e41cde6e064a2bed6af78e768c492d73f807
nedimorta <nedimorta@gmail.com>
2023-12-06 04:03:08 -0800

    WIP on FE

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ChannelForm from './components/ChannelForm';
 import ChannelList from './components/ChannelList';
+import ChannelAdjuster from './components/ChannelAdjuster';
 
 interface Channel {
   name: string;
@@ -11,13 +12,28 @@ const App: React.FC = () => {
   const [channels, setChannels] = useState<Channel[]>([]);
 
   useEffect(() => {
-    // Fetch channels from the backend
-    // ...
+    fetch('http://localhost:8080/api/channels')
+      .then(response => response.json())
+      .then(data => setChannels(data))
+      .catch(error => console.error('Error fetching channels:', error));
   }, []);
 
   const addChannel = (channel: Channel) => {
-    // Add channel to the backend
-    // ...
+    fetch('http://localhost:8080/api/channels', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(channel),
+    })
+    .then(response => response.json())
+    .then(newChannel => setChannels([...channels, newChannel]))
+    .catch(error => console.error('Error adding channel:', error));
+  };
+
+  const onAdjustChannelCount = (newCount: number) => {
+    // Implement the logic to update the channel count on the backend
+    // This might involve an API call to the backend
   };
 
   return (
@@ -25,6 +41,7 @@ const App: React.FC = () => {
       <h1>YouTube Channel Manager</h1>
       <ChannelForm onAddChannel={addChannel} />
       <ChannelList channels={channels} />
+      <ChannelAdjuster currentCount={channels.length} onAdjust={onAdjustChannelCount} />
     </div>
   );
 };
